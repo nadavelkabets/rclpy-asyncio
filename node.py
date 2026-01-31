3aae67feb468da380ba31737220009ea0e2fd1f2
Nadav Elkabets <elnadav12@gmail.com>
2026-01-31 21:52:12 +0000

    Add `__init__` to node class

--- node.py
+++ node.py
@@ -1,28 +1,86 @@
 import asyncio
+from typing import Optional, List, Union, Any, Dict
 
 from rclpy.node import Node
+from rclpy.context import Context
+from rclpy.qos import QoSProfile, qos_profile_rosout_default
+from rclpy.parameter import Parameter
 
 from .subscription import AsyncioSubscription
 from .service import AsyncioService
 from .client import AsyncioClient
 
 
+AsyncioEntity = Union[AsyncioService, AsyncioClient, AsyncioSubscription]
+
+
 class AsyncioNode(Node):
+    def __init__(
+        self,
+        node_name: str,
+        *,
+        context: Optional[Context] = None,
+        cli_args: Optional[List[str]] = None,
+        namespace: Optional[str] = None,
+        use_global_arguments: bool = True,
+        enable_rosout: bool = True,
+        rosout_qos_profile: Union[QoSProfile, int] = qos_profile_rosout_default,
+        start_parameter_services: bool = True,
+        parameter_overrides: Optional[List[Parameter[Any]]] = None,
+        allow_undeclared_parameters: bool = False,
+        automatically_declare_parameters_from_overrides: bool = False,
+        enable_logger_service: bool = False
+    ) -> None:
+        super().__init__(
+            node_name = node_name,
+            context = context,
+            cli_args = cli_args,
+            namespace = namespace,
+            use_global_arguments = use_global_arguments,
+            enable_rosout = enable_rosout,
+            rosout_qos_profile = rosout_qos_profile,
+            start_parameter_services = start_parameter_services,
+            parameter_overrides = parameter_overrides,
+            allow_undeclared_parameters = allow_undeclared_parameters,
+            automatically_declare_parameters_from_overrides = automatically_declare_parameters_from_overrides,
+            enable_logger_service = enable_logger_service
+        )
+
+        self._tg: Optional[asyncio.TaskGroup] = None
+        self._runners: Dict[AsyncioEntity, asyncio.Task] = {}
+              
+    async def __aenter__(self):
+        return self
+    
+    async def __aexit__(self, *_exc):
+        await self.close()
+
+    async def close(self):
+        for sub in self._subscriptions:
+            self.close_subscription(sub)
+        for srv in self._services:
+            self.close_service(srv)
+        for client in self._clients:
+            self.close_client(client)
+
     async def run(self):
-        async with asyncio.TaskGroup() as tg:
-            self._tg = tg
-            for sub in self._subscriptions:
-                task = tg.create_task(self._run_subscription(sub))
-                self._subscription_tasks[sub] = task
-            for srv in self._services:
-                task = tg.create_task(self._run_service(srv))
-                self._service_tasks[srv] = task
-            for client in self._clients:
-                task = tg.create_task(self._run_client(client))
-                self._client_tasks[client] = task
-        self._tg = None
+        try:
+            async with asyncio.TaskGroup() as tg:
+                self._tg = tg
+                for sub in self._subscriptions:
+                    task = tg.create_task(self._run_subscription(sub))
+                    self._runners[sub] = task
+                for srv in self._services:
+                    task = tg.create_task(self._run_service(srv))
+                    self._runners[srv] = task
+                for client in self._clients:
+                    task = tg.create_task(self._run_client(client))
+                    self._runners[client] = task
+        finally:
+            self._tg = None
 
     # TODO: do we want a concurrent=False flag that awaits the callback?
+    # TODO: do we want to utilize asyncio's eager_start on 3.12+?
     async def _run_subscription(self, subscription: AsyncioSubscription):
         """Node implements the read loop."""
         async with asyncio.TaskGroup() as tg:
@@ -57,7 +115,7 @@ class AsyncioNode(Node):
 
     def create_subscription(self, *args):
         sub = AsyncioSubscription(self)
-        self._subscriptions.add(sub)
+        self._subscriptions.append(sub)
         if self._tg:
             self._tg.create_task(self._run_subscription(sub))
 
@@ -65,7 +123,7 @@ class AsyncioNode(Node):
 
     def create_service(self, service_name, service_type, callback):
         srv = AsyncioService(self, service_name, service_type, callback)
-        self._services.add(srv)
+        self._services.append(srv)
         if self._tg:
             self._tg.create_task(self._run_service(srv))
             
@@ -73,17 +131,18 @@ class AsyncioNode(Node):
 
     def create_client(self, service_name, service_type):
         client = AsyncioClient(self, service_name, service_type)
-        self._clients.add(client)
+        self._clients.append(client)
         if self._tg:
             self._tg.create_task(self._run_client(client))
 
         return client
 
+    # TODO: do we want a graceful=False flag that cancels all pending callbacks?
     async def close_subscription(self, sub):
         """
         Stop processing new messages and wait for existing callbacks to complete.
         """
-        task = self._subscription_tasks.pop(sub)
+        task = self._runners.pop(sub)
         sub.close()
         await task
 
@@ -94,7 +153,7 @@ class AsyncioNode(Node):
         """
         Stop processing new requests and wait for existing callbacks to complete.
         """
-        task = self._service_tasks.pop(srv)
+        task = self._runners.pop(srv)
         srv.close()
         await task
 
@@ -105,7 +164,7 @@ class AsyncioNode(Node):
         """
         Stop allowing new calls and cancel all pending calls.
         """
-        task = self._client_tasks.pop(client)
+        task = self._runners.pop(client)
         client.close()
         await task
 
